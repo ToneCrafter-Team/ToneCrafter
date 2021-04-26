0d551ce5273bc991fabc01d7e555a02330292d93
KingFaolan <49042144+KingFaolan@users.noreply.github.com>
2021-04-26 10:43:16 +0200

    Add files via upload

--- VAE/MusicToPNG.py
+++ VAE/MusicToPNG.py
@@ -8,10 +8,12 @@ x=librosa.load(filename)
 
 hop_length = 256
 
-fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+fig, (ax1,ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 D = librosa.amplitude_to_db(np.abs(librosa.stft(x[0],n_fft=2048,hop_length=hop_length)), ref=np.max)
 
 img = librosa.display.specshow(D, y_axis='linear', x_axis='time',
-                               sr=x[1], ax=ax[0], hop_length=hop_length)
-ax[0].set(title='Linear-frequency power spectrogram')
-ax[0].label_outer()
+                               sr=x[1], ax=ax1, hop_length=hop_length)
+ax1.set(title='Linear-frequency power spectrogram')
+ax1.label_outer()
+
+librosa.display.waveplot(x[0], sr=x[1], ax=ax2)
